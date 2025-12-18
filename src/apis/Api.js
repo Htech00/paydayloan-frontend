a4205341e87f4be11a4069f5b03af0d61d63b231
Htech00 <htechsolutionz@gmail.com>
2025-12-18 10:18:47 +0100

    first commit

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -3,13 +3,13 @@ import axios from "axios";
 // const baseURL = "http://sapphire-loan.test";
 // const baseURL = "http://127.0.0.1:8000";
 
-const baseURL = "http://192.168.1.46";
+const baseURL = "/";
 // const baseURL = "https://api-pro.sentiflex.com";
 
 let Api = axios.create({
   baseURL: baseURL,
   headers: {
-    'x-sentinel-key': '6552a219-b30a-4764-95b4-d7996a74ed7c'
+    'x-sentinel-key': '050adbc2-4805-4684-8b92-722412dc1387'
   }
 });
 
